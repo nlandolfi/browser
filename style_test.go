5ff2c0d147f7e964a9fee764a0af6ce3b1f47938
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2022-10-27 15:18:36 -0700

    update test and README

--- style_test.go
+++ style_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestExample(t *testing.T) {
 	s := Size{Value: 100, Unit: UnitPX}
 
-	if got, want := s.String(), "100px"; got != want {
+	if got, want := s.String(), "100.000000px"; got != want {
 		t.Fatalf("got %s, want %s", got, want)
 	}
 }
