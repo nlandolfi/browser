99c9f72026f465e5baf093c529ec018c4c8700d5
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2023-01-24 21:25:25 -0800

    add some comments

--- browser.go
+++ browser.go
@@ -17,13 +17,23 @@ type LocalStorage interface {
 	Del(k string)
 }
 
-type Event interface{}
-
+// Events is the global events channel.
 var Events = make(chan Event)
 
-// call like:
-// go browser.Dispatch(e)
-// a hack to force a re-render is: go browser.Dispatch(nil)
+// Event is a generic helper typer for an event object.
+//
+// Usually code type switches on these for handling.
+type Event interface{}
+
+// Use Dispatch to add an event to the queue. Usually you want to
+// do so in in a non-blocking way, in which case, use the `go` keyword
+//
+//   go browser.Dispatch(e)
+//
+// A simple 'hack' to force a re-render is to dispatch a nil event
+//
+//   go browser.Dispatch(nil)
+//
 func Dispatch(e Event) {
 	Events <- e
 }
